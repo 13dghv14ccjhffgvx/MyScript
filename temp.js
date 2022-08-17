7b8562b8ae3554280a42af3944a1fb7eda120491
小白 <2136585216@qq.com>
2022-08-17 11:43:48 +0800

    commit

---
+++ temp.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         JunYi
+// @namespace    https://Junyi.com
+// @version      0.1
+// @description  The frist script
+// @author       JunYi
+// @match        *://*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log('Hello,world');
+})();
