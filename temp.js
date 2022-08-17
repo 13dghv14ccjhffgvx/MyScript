87183e4634a84286a9881d9e8110645869e77912
小白 <2136585216@qq.com>
2022-08-17 15:41:25 +0800

    update

--- temp.js
+++ temp.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         JunYi
 // @namespace    https://Junyi.com
-// @version      0.1
+// @version      2.0
 // @description  The frist script
 // @author       JunYi
 // @match        *://*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
 // @license           AGPL License
+// @icon         https://static.zhihu.com/heifetz/favicon.ico
 // @grant        none
 // ==/UserScript==
 
